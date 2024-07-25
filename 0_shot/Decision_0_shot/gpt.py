a5aa78707ea2b699f6e094cc466afd6740cee7b9
Amey Karan <ameykaran2005@gmail.com>
2024-07-25 17:32:25 +0530

    Added generation time

--- 0_shot/Decision_0_shot/gpt.py
+++ 0_shot/Decision_0_shot/gpt.py
@@ -5,6 +5,13 @@ from dotenv import load_dotenv
 import pandas as pd
 import argparse
 from tqdm import tqdm
+from datetime import datetime
+
+INPUT_FILE = "../../Data/ADR-data/data10.jsonl"
+
+# MODEL_NAME = 'gpt-4o'
+MODEL_NAME = 'gpt-3.5-turbo'
+# MODEL_NAME = 'babbage-002'
 
 
 def load_jsonl(file_path):
@@ -15,9 +22,12 @@ def load_jsonl(file_path):
     return data
 
 
-def save_jsonl(data: pd.DataFrame, file_path):
-    with open(file_path, 'a') as file:
-        file.write(data.to_json(orient='records', lines=True))
+def save_jsonl(data: pd.DataFrame, file_path, append=True):
+    if append:
+        with open(file_path, 'a') as file:
+            file.write(data.to_json(orient='records', lines=True))
+    else:
+        data.to_json(file_path, orient='records', lines=True)
 
 
 def get_data(data: pd.DataFrame, max_length=-1):
@@ -42,18 +52,14 @@ system_message = "This is an Architectural Decision Record for a software. Give 
 
 openai.api_key = os.getenv('OPENAI_KEY')
 
-CHAT_MODELS = ["gpt-4o", "gpt-3.5-turbo"]
-COMPLETION_MODELS = ["babbage-002"]
-
-# MODEL_NAME = "gpt-4o"
-MODEL_NAME = "gpt-3.5-turbo"
-# MODEL_NAME = "babbage-002"
+CHAT_MODELS = ['gpt-4o', 'gpt-3.5-turbo']
+COMPLETION_MODELS = ['babbage-002']
 
 
 def run(start=0, days=10000):
-    data = pd.read_json("../../Data/ADR-data/data100.jsonl", lines=True)
-    print(f"Generating predictions using {MODEL_NAME}")
-    print(f"Total records: {len(data)}")
+    data = pd.read_json(INPUT_FILE, lines=True)
+    print(f'Generating predictions using {MODEL_NAME}')
+    print(f'Total records: {len(data)}')
 
     data, removed = get_data(data)
     done = 0
@@ -62,7 +68,7 @@ def run(start=0, days=10000):
 
     failed = []
 
-    for i, row in tqdm(data[start:start+days].iterrows(), total=total):
+    for i, row in tqdm(data[start: start + days].iterrows(), total=total):
         if i in removed:
             continue
 
@@ -71,8 +77,8 @@ def run(start=0, days=10000):
                 response = openai.chat.completions.create(
                     model=MODEL_NAME,
                     messages=[
-                        {"role": "system", "content": system_message},
-                        {"role": "user", "content": row['Context']}
+                        {'role': 'system', 'content': system_message},
+                        {'role': 'user', 'content': row['Context']},
                     ],
                     temperature=1,
                     max_tokens=2500,
@@ -84,9 +90,10 @@ def run(start=0, days=10000):
 
             elif MODEL_NAME in COMPLETION_MODELS:
                 response = openai.completions.create(
-                    model="babbage-002",
-                    prompt="This is an Architectural Decision Record for a software" +
-                    row['Context'] + "\n## Decision\n",
+                    model='babbage-002',
+                    prompt='This is an Architectural Decision Record for a software'
+                    + row['Context']
+                    + '\n## Decision\n',
                     temperature=1,
                     max_tokens=2500,
                     top_p=1,
@@ -96,26 +103,26 @@ def run(start=0, days=10000):
                 row['Predicted'] = response.choices[0].text
 
             else:
-                raise KeyError("Invalid model")
+                raise KeyError('Invalid model')
 
+            row['GenTime'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
             row = row.to_frame().T
-            save_jsonl(
-                row, f"../results/{MODEL_NAME}_{start}_{days}.jsonl")
+            save_jsonl(row, f'../results/{MODEL_NAME}_{start}_{days}.jsonl')
             done += 1
 
         except Exception as e:
-            print(f"Exception: {e}")
-            print(f"Failed for {row['id']}")
+            print(f'Exception: {e}')
+            print(f'Failed for {row['id']}')
             failed.append((row['id'], e))
 
-    print(f"Done for {done} records")
+    print(f'Done for {done} records')
 
     if len(failed) > 0:
         failed_df = data[data['id'].isin([f[0] for f in failed])]
         failed_df['Error'] = [f[1] for f in failed]
         save_jsonl(
-            failed_df, f"../results/{MODEL_NAME}_failed_{start}_{days}.jsonl")
-        print(f"Failed for {len(failed)} records")
+            failed_df, f'../results/{MODEL_NAME}_failed_{start}_{days}.jsonl', False)
+        print(f'Failed for {len(failed)} records')
 
 
 if __name__ == "__main__":
