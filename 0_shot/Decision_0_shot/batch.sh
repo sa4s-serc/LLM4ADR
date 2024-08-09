79211d89fd921399ed49667ecba0ba61e6731f67
adyanshkakran <adyansh.kakran@research.iiit.ac.in>
2024-08-09 18:54:24 +0530

    gemma done, RAG done, setting up new approach

--- 0_shot/Decision_0_shot/batch.sh
+++ 0_shot/Decision_0_shot/batch.sh
@@ -3,7 +3,6 @@
 #SBATCH --gres=gpu:2
 #SBATCH --output=output.txt
 #SBATCH --time=48:00:00
-#SBATCH -w gnode057
 
 source /home2/adyansh/miniconda3/bin/activate
 
@@ -11,7 +10,7 @@ source /home2/adyansh/miniconda3/bin/activate
 mkdir -p /scratch/adyansh/cache
 conda activate /home2/adyansh/LLM4ADR/research
 
-python3 /home2/adyansh/LLM4ADR/0_shot/Decision_0_shot/gemma.py --num_left 1000
+python3 /home2/adyansh/LLM4ADR/0_shot/Decision_0_shot/llama.py --num_left 1000 --start 2500
 # python3 /home2/adyansh/LLM4ADR/0_shot/Decision_0_shot/score.py flan-t5-base
 # python3 /home2/adyansh/LLM4ADR/0_shot/Decision_0_shot/score.py flan-t5-small
 # python3 /home2/adyansh/LLM4ADR/0_shot/Decision_0_shot/score.py t5-small
