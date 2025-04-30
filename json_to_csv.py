00b22c2cac8fefae504f4602c4ddda7a184b236e
rudradhariiit <YOUR_EMAIL>
2025-04-30 18:50:16 +0530

    removed unnecessary files

--- json_to_csv.py
+++
@@ -1,7 +0,0 @@
-import json
-import sys
-
-json_file = sys.argv[1]
-data = json.load(open(json_file))
-
-print(data["rouge"]["rouge1"], data["rouge"]["rouge2"], data["bleu"]["bleu"], data["bleu"]["precisions"][0], data["bleu"]["precisions"][1], data["meteor"]["meteor"], data["bertscore"]["precision"], data["bertscore"]["recall"], data["bertscore"]["f1"], sep=",")
